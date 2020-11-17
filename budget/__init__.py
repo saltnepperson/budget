074a0ccebc2fd7ba1d86ad03e37ab0eb36062e32
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-17 12:21:17 -0500

    Initialization

---
+++ budget/__init__.py
@@ -0,0 +1,28 @@
+import os
+import stat
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+
+# Get that environment configuration
+if os.environ.get('BUDGET_SETTINGS'):
+    app.config.from_envvar('BUDGET_SETTINGS')
+else:
+    from os.path import dirname, join, isfile
+    configFile = dirname(dirname(__file__))
+    configFile = join(configFile, 'env-config')
+    configFile = join(configFile, 'config.py')
+
+    if isfile(configFile):
+        app.config.from_pyfile(configFile)
+    else:
+        print('We need a config file to run this application')
+        exit(-1)
+
+### Flask API ###
+from flask_restful import Api
+api = Api(app)
---
+++ budget/models/users.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLALchemy
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Unicode, Text
+from sqlalchemy.orm import relationship
+from budget import app, db
+
+# The users table stores all of the budget applications users
+class User(db.Model):
+    """
+    Represents a Budget app user.
+    """
+    __tablename__ = 'users'
+
+    # Basic user info
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    middle_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False, index=True)
+    username = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    password = db.Column(db.String(255), nullable=False)
+    
+    # Tracking information
+    last_login = Column(DateTime())
+    last_updated = Column(DateTime())
+
+    # Relationships 
+    budgets = relationship("Bugets")
+
+    def __init__(self, first_name, last_name, email, username, password, middle_name=None, last_login=None, last_updated=None):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = password
+        self.last_login = last_login
+        self.last_updated = last_updated
+
+    def __str__(self):
+        return '<User id=%s email=%s>' % (self.id, self.email)
+
