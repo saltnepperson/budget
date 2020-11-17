b907fa59b3fa001c09fb72f6f42e7cde3c6b3426
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-17 12:21:45 -0500

    Initial local environment configuration

---
+++ env-config/config.py
@@ -0,0 +1,11 @@
+ENVIRONMENT = 'local'
+
+# Database configuration
+SQLALCHEMY_DATABASE_URI = 'postgresql://budget_user:thisisasecurepassword@localhost:5432/budget_dev'
+
+# Api configuration
+API_PORT = 5000
+API_HOST = 'localhost'
+API_PATH = '/api/'
+
+BASE_URL = 'http://{}:{}{}'.format(API_HOST, API_PORT, API_PATH)
