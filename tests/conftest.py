ee9f03e22506db7eb5b523f0d5e671a2d340e65c
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-22 23:45:41 -0500

    Add test budget item

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from unittest import mock
 from budget import app, db
 from budget.models.users import User
 from budget.models.budget import Budget
+from budget.models.budget_item import BudgetItem
 
 @pytest.fixture(scope='module')
 def api_client():
@@ -42,4 +43,17 @@ def test_budget(api_client, test_user):
     yield budget
 
     db.session.delete(budget)
+    db.session.commit()
+
+# Create a budget item to test with
+@pytest.fixture(scope='module')
+def test_budget_item(api_client, test_user, test_budget):
+    budget_item = BudgetItem(name='Test Budget Item', description='Test test test test.', category='Test Item', created_by=test_user.id, budget_id=test_budget.id)
+
+    db.session.add(budget_item)
+    db.session.commit()
+
+    yield budget
+
+    db.session.delete(budget_item)
     db.session.commit()
